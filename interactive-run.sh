a2841b27509130f5d239b787b49b2d5f316dd557
nialljb <nialljbourke@gmail.com>
2025-06-20 14:43:12 +0100

    debug

---
+++ interactive-run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash 
+
+GEAR=fw-GAMBAS
+IMAGE=flywheel/gambas:BETA.0.0.4
+LOG=gambas-BETA.0.0.4-685555153505adf53177084b
+
+# Command:
+docker run -it --rm --entrypoint bash\
+	-v /Users/nbourke/GD/atom/unity/fw-gears/${GEAR}/app/:/flywheel/v0/app\
+	-v /Users/nbourke/GD/atom/unity/fw-gears/${GEAR}/utils:/flywheel/v0/utils\
+	-v /Users/nbourke/GD/atom/unity/fw-gears/${GEAR}/run.py:/flywheel/v0/run.py\
+	-v /Users/nbourke/GD/atom/unity/fw-gears/${GEAR}/${LOG}/input:/flywheel/v0/input\
+	-v /Users/nbourke/GD/atom/unity/fw-gears/${GEAR}/${LOG}/output:/flywheel/v0/output\
+	-v /Users/nbourke/GD/atom/unity/fw-gears/${GEAR}/${LOG}/work:/flywheel/v0/work\
+	-v /Users/nbourke/GD/atom/unity/fw-gears/${GEAR}/${LOG}/config.json:/flywheel/v0/config.json\
+	$IMAGE
+
+
